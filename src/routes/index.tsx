abd7af5a9dbc8e4a72f65320d5f39e768281143a
George Fischer <me@george.id.au>
2023-01-23 11:44:24 +0100

    arc timer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,15 +18,22 @@ export default function Home() {
     whenIsNextRockerTime().getTime() - Date.now()
   );
 
+  const now = new Date();
+  const [isRockerTime, setIsRockerTime] = createSignal(
+    now.getHours() == 19 && now.getMinutes() == 4
+  );
+
   createEffect(() => {
     let canceled = false;
 
     function update() {
       setRockerTime(whenIsNextRockerTime().getTime() - Date.now());
+      const now = new Date();
+      setIsRockerTime(now.getHours() == 19 && now.getMinutes() == 4);
 
-      document.title = `${ms(rockerTime(), { long: true })} until rocker time`;
+      document.title = `${ms(2, { long: true })} until rocker time`;
 
-      if (!canceled) requestAnimationFrame(update);
+      if (!canceled) setTimeout(update, 1000);
     }
 
     requestAnimationFrame(update);
@@ -34,15 +41,29 @@ export default function Home() {
     () => (canceled = true);
   });
 
-  const now = new Date();
-  const isRockerTime = now.getHours() == 19 && now.getMinutes() == 4;
+  const day = ms("1 day");
 
-  return isRockerTime ? (
+  const r = 45;
+  const c = Math.PI * (r * 2);
+  return isRockerTime() ? (
     <h1 class="text-7xl mx-auto w-max p-12">Rocker time!!!</h1>
   ) : (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      {ms(rockerTime(), { long: true })} until rocker time
-      <progress max={86400000} value={rockerTime()} />
+    <main class="h-full w-full flex items-center justify-center flex-col gap-2">
+      <svg class="w-80 h-80 absolute" viewBox="-60 -60 120 120">
+        <circle r={45} stroke-width={2} fill="transparent" stroke="#0001" />
+        <circle
+          r={r}
+          stroke-width={2}
+          stroke-dasharray={`${Math.PI * 45 * 2}`}
+          stroke-dashoffset={`${(1 - rockerTime() / day) * c}px`}
+          fill="transparent"
+          stroke-linecap="round"
+          class="text-blue-600 transition-all -rotate-90"
+          stroke="currentcolor"
+        />
+      </svg>
+      <div class="text-4xl">{ms(rockerTime(), { long: true })}</div>
+      <div class="relative text-lg"> until rocker time</div>
     </main>
   );
 }
